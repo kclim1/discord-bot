b5284bbd0f8bf8ba65bb4891d851c2b52ad36c13
KC Lim <kclim0528@gmail.com>
2024-06-29 06:57:11 -0700

    configured the app to use gtp model 3.5 turbo

--- index.js
+++ index.js
@@ -18,7 +18,24 @@ const openai = new OpenAI({
     apiKey : process.env.OPENAI_API_KEY
 })
 
-
+client.on('messageCreate',async(message)=>{
+    if(message.author.bot){return}
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages:[
+            {
+                // name:
+                role:'system',
+                content: 'ChatGPT is a friendly bot'
+            },
+            {
+                role:'user',
+                content:message.content
+            }
+        ]
+    }).catch((error)=>console.error('openai Error:\n',error))
+    message.reply(response.choices[0].message.content)
+})
 client.on(Events.MessageCreate, (msg) => {
   if (msg.content === "hello bot") {
     console.log(msg.author.username)
